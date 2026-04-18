6bdc4efa6d62299a6a6055ec9910268555be4247
Tobias Molinero <tobiasmolinero98@gmail.com>
2026-04-17 21:18:38 -0300

    fix(eslint): correct configuration issue

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import prettierConfig from "eslint-config-prettier";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  ...prettierConfig,
+  prettierConfig,
 
   {
     rules: {
