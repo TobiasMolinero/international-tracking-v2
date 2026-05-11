706149700624ce6255a2a76e2188cbcac4bd0745
Tobias Molinero <tobiasmolinero98@gmail.com>
2026-05-11 11:59:12 -0300

    hotfix(tracking): fix bug formatter date

--- components/tracking/TrackingData.tsx
+++ components/tracking/TrackingData.tsx
@@ -1,5 +1,4 @@
 import { Shipment } from "@/types/tracking";
-import { formatDate } from "@/lib/formatters";
 
 export const TrackingData = ({shipmentData}: {shipmentData: Shipment}) => {
     return (
@@ -7,7 +6,7 @@ export const TrackingData = ({shipmentData}: {shipmentData: Shipment}) => {
             <h3 className="font-bold text-xl mb-2">Datos del Envío</h3>
             <div className="flex flex-col gap-y-1">
                 <p><span className="font-bold">Número de Seguimiento:</span> {shipmentData.nro_venta}</p>
-                <p><span className="font-bold">Fecha de envio:</span> {formatDate(shipmentData.fecha_envio)}</p>
+                <p><span className="font-bold">Fecha de envio:</span> {shipmentData.fecha_envio}</p>
                 <p><span className="font-bold">Nombre consignatario:</span> {shipmentData.nombre_consignatario}</p>
                 <p><span className="font-bold">Nro. Identidad:</span> {shipmentData.carnet_identidad}</p>
                 <p><span className="font-bold">HBL:</span> {shipmentData.hbl}</p>
